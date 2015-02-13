2c8b6039c3547fb0b035c779ff92894e210c760e
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-02-13 00:11:52 -0600

    Update loris.py
    The header was added

--- loris.py
+++ loris.py
@@ -1,1 +1,11 @@
-#First line
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Loris is a simple micro-framework for small web applications written in a 
+single file and with no dependencies other than the Python Standard Library.
+
+Homepage and documentation: no available yet.
+
+Copyright (c) 2015, Tarcisio Bruno C. Oliveira
+"""
