f936fa543d9d55816e41f04ad6ed23c240474f8a
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2015-02-19 00:29:22 -0600

    Update loris.py
    First lines! This code runs a Hello World server.

--- loris.py
+++ loris.py
@@ -11,3 +11,11 @@ Homepage and documentation: no available yet.
 
 Copyright (c) 2015, Tarcisio Bruno C. Oliveira
 """
+
+def app(environ, start_response):
+    start_response('200 OK', [('content-type', 'text/html')])
+    return ['Hello world!']
+
+if __name__ == '__main__':
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
